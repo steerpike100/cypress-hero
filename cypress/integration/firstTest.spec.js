d7deac1b96de11b575dcf2d7e576d92649ee2bfc
steveb <stevejbartholomew@gmail.com>
2020-04-05 11:02:56 +0100

    Seventh commit

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -146,23 +146,39 @@ describe("Our second suite", () => {
     //     });
   });
 
-  it("assert property", () => {
+  it.only("assert property", () => {
+
+    function selectDayFromCurrent(day){
+
+      let date = new Date()
+      date.setDate(date.getDate() + day)
+      let futureDay = date.getDate()
+      let futureMonth = date.toLocaleString('default', {month: 'short'})
+      let dateAssert = futureMonth + ' ' + futureDay + ', ' + date.getFullYear()
+
+      cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+        if(!dateAttribute.includes(futureMonth)){
+          cy.get('[data-name="chevron-right"]').click()
+          selectDayFromCurrent(day)
+        }else{
+          cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+        }
+    })
+    return dateAssert
+  }
     cy.visit("/");
 
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
-    cy.contains("nb-card", "Common Datepicker")
-      .find("input")
-      .then(input => {
-        cy.wrap(input).click();
-        cy.get("nb-calendar-day-picker")
-          .contains("30")
-          .click();
-        cy.wrap(input)
-          .invoke("prop", "value")
-          .should("contain", "Mar 30, 2020");
-      });
-  });
+
+    
+    cy.contains("nb-card", "Common Datepicker").find("input").then(input => {
+      cy.wrap(input).click()
+      let dateAssert = selectDayFromCurrent(60)
+      cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+      })
+    })
+
 
   it("radio buttons", () => {
     cy.visit("/");
@@ -244,7 +260,7 @@ describe("Our second suite", () => {
       });
     });
   });
-  it.only("Web tables", () => {
+  it("Web tables", () => {
     cy.visit("/");
 
     cy.contains("Tables & Data").click();
